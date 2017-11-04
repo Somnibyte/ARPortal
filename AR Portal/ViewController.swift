5c0f9d84a4945c0ab1d141b7e7f1ad391e3037e3
Guled <somnibyte@gmail.com>
2017-11-04 19:41:58 -0400

    Initial Commit

---
+++ AR Portal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AR Portal
+//
+//  Created by Guled on 11/4/17.
+//  Copyright © 2017 Somnibyte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
